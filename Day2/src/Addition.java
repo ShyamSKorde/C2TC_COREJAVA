0a71c59a009926ff7ae6dd27eb3606d69c49dff7
shyam <shyam@LAPTOP-DE3TSDHJ>
2022-03-03 16:45:36 +0530

    First Commit

---
+++ Day2/src/Addition.java
@@ -0,0 +1,12 @@
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int A = 10;
+        int B = 20;
+        int C = A+B;
+        System.out.println("the sum of A & B is"+C);
+	}
+
+}
