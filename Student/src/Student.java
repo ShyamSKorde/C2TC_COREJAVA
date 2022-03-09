7c8dc5ce17024a65d9c016e5b49b9b7948043203
shyam <shyam@LAPTOP-DE3TSDHJ>
2022-03-09 13:20:11 +0530

    normal program in class and object, method

---
+++ Student/src/Student.java
@@ -0,0 +1,22 @@
+public class Student 
+{
+	int RollNo;
+	String Name;  
+	public void display()
+	{
+		System.out.println(" I am the student of "); 
+	}
+	public void stud1()
+	{
+		System.out.println("Dr. Babasaheb Ambedkar Marathavada University ");
+	}
+	public static void main(String[] args)
+	{
+		Student Std = new Student();
+		Std.display();
+		Std.stud1(); 
+			// TODO Auto-generated method stub
+
+	}
+
+}
