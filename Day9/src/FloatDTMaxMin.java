e07246121dca0a6a8276744b9592450ec12904e9
shyam <shyam@192.168.1.35>
2022-03-13 15:16:49 +0530

    Program Data Type print maximum and minimum values of float and double

---
+++ Day9/src/FloatDTMaxMin.java
@@ -0,0 +1,21 @@
+public class FloatDTMaxMin 
+{
+	public static void main(String[] args) 
+	{
+		float Float1 = 1249f/526f;
+		float Float2 = 253585868988686896686855906694093398415f;
+		float Float3 = 785.47854894387587754389878987766677887f;
+		double Double1 = 1249d/526d;
+		double Double2 = 253585868988686896686855906694093398415d;
+		double Double3 = 785.47854894387587754389878987766677887d;
+		System.out.println(" float valu of Float1 is := "+ Float1);
+		System.out.println(" float valu of Float2 is := "+ Float2);
+		System.out.println(" float valu of Float3 is := "+ Float3);
+		System.out.println(" double valu of Double1 is := "+ Double1);
+		System.out.println(" double valu of Double2 is := "+ Double2);
+		System.out.println(" double valu of Double3 is := "+ Double3);
+	}
+}
+
+
+
