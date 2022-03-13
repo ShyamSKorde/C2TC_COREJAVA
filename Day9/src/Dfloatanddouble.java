a7c3b570ae3c3a5c02bcd46a785ddd19575c373f
shyam <shyam@192.168.1.35>
2022-03-13 15:18:08 +0530

    simple program of print difference between float and double Data Type

---
+++ Day9/src/Dfloatanddouble.java
@@ -0,0 +1,13 @@
+public class Dfloatanddouble 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Difference between float and Double data type is : = ");
+		System.out.println("----float----"+"\n"+"Floating-Point Data Type : = float"+"\n"+"Values : = IEEE 754 Floating-Point"+"\n"+"Size := 32 bits"+"\n"+"Storage := 4 bytes"+"\n"+"Default Value : =0.0f"+"\n"+"Precision := Single"+"\n"+"Decimal := 5 to 6 Decimal digits"+"\n"+"Range := 3.4e-038 to 3.4e+038"+"\n"+"Accuracy := Low");
+		System.out.println("----double----"+"\n"+"Floating-Point Data Type : = double"+"\n"+"Values : = IEEE 754 Floating-Point"+"\n"+"Size := 32 bits"+"\n"+"Storage := 4 bytes"+"\n"+"Default Value : =0.0d"+"\n"+"Precision := Double"+"\n"+"Decimal := 15 to 16 Decimal digits"+"\n"+"Range := 1.7e-308 to 1.7e+308"+"\n"+"Accuracy := High");
+		// TODO Auto-generated method stub
+		
+	}
+
+}
