847d17d387fadc2685e38178a72e7a4783b885ac
shyam <shyam@192.168.1.35>
2022-03-13 15:14:54 +0530

    Integer Data type print Maximum and Minimum values

---
+++ Day9/src/IntegerDTMaxMin.java
@@ -0,0 +1,22 @@
+
+public class IntegerDTMaxMin {
+
+	public static void main(String[] args) 
+	{
+		byte byteMax = 127;
+		byte byteMin = -128;
+		short shortMax = 32767;
+		short shortMin = -32768;
+		int intMax = 2147483647;
+		int intMin = -2147483648;
+		long longMax = 9223372036854775807L;
+		long longMin = -9223372036854775808L;
+        System.out.println("byte Maximum Size is := "+byteMax+"  to  "+"byte Minimum Size is := "+byteMin);
+        System.out.println("short Maximum Size is := "+shortMax+"  to  "+"short Minimum Size is := "+shortMin);
+        System.out.println("integer Maximum Size is := "+intMax+"  to  "+"integer Minimum Size is := "+intMin);
+        System.out.println("long Maximum Size is := "+longMax+"  to  "+"long Minimum Size is := "+longMin);
+		// TODO Auto-generated method stub
+
+	}
+
+}
