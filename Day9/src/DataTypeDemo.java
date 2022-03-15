ca2ee705386130983c2773e926e24d68e41683d1
shyam <shyam@LAPTOP-DE3TSDHJ>
2022-03-15 13:58:46 +0530

    Simple Data Type Demo Program

---
+++ Day9/src/DataTypeDemo.java
@@ -0,0 +1,25 @@
+public class DataTypeDemo {
+	
+	public static void main (String args[])
+	{
+		int var1 =474;
+		float var2 = 345.8f;
+		double var3 = 26.9;
+		char  var4 = 'S';
+		boolean var5 = true;
+		byte var6 = 127;
+		short var7 = 7855;
+		long var8 = 9585L;
+		
+		System.out.println(var1+"\n");
+		System.out.println(var2+"\n");
+		System.out.println(var3+"\n");
+		System.out.println(var4+"\n");
+		System.out.println(var5+"\n");
+		System.out.println(var6+"\n");
+		System.out.println(var7+"\n");
+		System.out.println(var8+"\n");
+	}
+
+
+}
